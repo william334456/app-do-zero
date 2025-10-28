24dd448ce6f78288d9004ec0bd70fc75365b5e91
William334456 <nathan334456@gmail.com>
2025-10-28 20:37:24 -0300

    feat: expo app base + firebase + upload base64

---
+++ my-app/firebaseConfig.ts
@@ -0,0 +1,10 @@
+// firebaseConfig.ts
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyDNy1afOI_9qiaULoQCM75Ov0IOZv-zU",
+  authDomain: "app-do-zero-13dd6.firebaseapp.com",
+  projectId: "app-do-zero-13dd6",
+  storageBucket: "app-do-zero-13dd6.appspot.com", // ✅ corrigido aqui
+  messagingSenderId: "942858605393",
+  appId: "1:942858605393:web:b322ca31059707f0a8b970",
+};
